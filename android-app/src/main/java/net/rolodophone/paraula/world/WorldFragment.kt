64018a0771ae0c5455def7006094290fcffd0011
Sergi Lange-Soler <sergi.lange123@gmail.com>
2020-07-18 22:26:01 +0100

    added basic world view

--- android-app/src/main/java/net/rolodophone/paraula/world/WorldFragment.kt
+++ android-app/src/main/java/net/rolodophone/paraula/world/WorldFragment.kt
@@ -1,9 +1,11 @@
 package net.rolodophone.paraula.world
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -11,11 +13,16 @@ import net.rolodophone.paraula.R
 import net.rolodophone.paraula.databinding.WorldFragmentBinding
 
 class WorldFragment : Fragment() {
+    lateinit var binding: WorldFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<WorldFragmentBinding>(inflater, R.layout.world_fragment, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.world_fragment, container, false)
 
         binding.backButton.setOnClickListener { findNavController().navigateUp() }
+        binding.worldView.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.world_background))
+
+        Log.i("WorldFragment","onCreateView called")
 
         return binding.root
     }
