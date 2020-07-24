40b0633bac8110a21d42c1fb335c6f6ce775e909
Sergi Lange-Soler <sergi.lange123@gmail.com>
2020-07-24 14:31:57 +0100

    added up navigation in learning activity

--- android-app/src/main/java/net/rolodophone/paraula/learning/LearningActivity.kt
+++ android-app/src/main/java/net/rolodophone/paraula/learning/LearningActivity.kt
@@ -1,6 +1,7 @@
 package net.rolodophone.paraula.learning
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import net.rolodophone.paraula.R
 
@@ -9,5 +10,10 @@ class LearningActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.learning_activity)
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun up(view: View) {
+        finish()
     }
 }
